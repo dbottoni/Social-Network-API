603b4cf6101e9b1bd32d9bf50245c87f2624e065
Dana Bottoni <danabottoni@gmail.com>
2021-09-16 13:30:09 -0400

    setup User model

---
+++ models/Thought.js
@@ -0,0 +1,8 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+
+
+const Thought = model('Thought', ThoughtSchema)
+
+module.exports = User;
